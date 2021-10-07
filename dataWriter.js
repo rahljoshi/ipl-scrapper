60c615ff69df9da86d5b463cf59046f2a95b9393
Rahul Joshi <mrjoshi0147@gmail.com>
2021-10-08 00:26:02 +0530

    IPL scrapper

---
+++ dataWriter.js
@@ -0,0 +1,11 @@
+// const xlsx = require("xlsx");
+// const fs = require("fs");
+
+// function dataWriter(filePath, data, sheetName) {
+//   let newWB = xlsx.utils.book_new();
+//   let newWS = xlsx.utils.json_to_sheet(data);
+//   xlsx.utils.book_append_sheet(newWB, newWS, sheetName);
+//   xlsx.writeFile(newWB, filePath);
+// }
+
+// module.exports = { excelWriter };
